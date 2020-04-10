c55ae75f29c835a1d1805d988c1ed63debec02da
mewprakarn <51776670+mewprakarn@users.noreply.github.com>
2020-04-10 17:22:35 +0700

    Add Keypress interaction

--- spaceship.py
+++ spaceship.py
@@ -13,7 +13,18 @@ class Ship:
 
         # Start each new ship at the bottome center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
-    
+
+        # Movement Flag
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Update the ship's position based on the movement flag."""
+        if self.moving_right:
+            self.rect.x +=1
+        if self.moving_left:
+            self.rect.x -=1
+
     def blitme(self):
         """Draw ship at its current location"""
         self.screen.blit(self.image, self.rect)
