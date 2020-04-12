5fc8be8f41f15e439daa1550b09a7c37ec7e4182
mewprakarn <51776670+mewprakarn@users.noreply.github.com>
2020-04-12 14:59:54 +0700

    Moving Aliens fleet

--- Alien_invasion.py
+++ Alien_invasion.py
@@ -35,6 +35,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
             # Get rid of bullets that have disappeared.
@@ -125,6 +126,26 @@ class AlienInvasion:
         alien.rect.x = alien.x 
         alien.rect.y = alien.rect.height +2*alien.rect.height*row_number
         self.aliens.add(alien)
+    
+    def _update_aliens(self):
+        """
+        Check if the fleet is at an edge,
+            then update the positions of all aliens in the fleet."""
+        self._check_fleet_edges()
+        self.aliens.update()
+
+    def _check_fleet_edges(self):
+        """Response appropriately if any aliens have reached an edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Drop entire fleet and change the fleet's direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
