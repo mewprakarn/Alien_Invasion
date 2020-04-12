447ca25bff7d9b3edc19309b72a332aaee82b6d7
mewprakarn <51776670+mewprakarn@users.noreply.github.com>
2020-04-12 13:45:58 +0700

    Add Alien fleet

--- Alien_invasion.py
+++ Alien_invasion.py
@@ -87,12 +87,7 @@ class AlienInvasion:
         if len(self.bullets) < self.settings.bullet_allowed:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
-
-    def _create_fleet(self):
-        """Create the fleet to aliens."""
-        alien = Alien(self)
-        self.aliens.add(alien)
-        
+      
     def _update_screen(self):
         """Update image on the screen, and flip to new screen."""
         self.screen.fill(self.settings.bg_color)
@@ -103,6 +98,33 @@ class AlienInvasion:
         self.aliens.draw(self.screen)
         # Make the most recently drawn screen visible.
         pygame.display.flip()
+
+    def _create_fleet(self):
+        """Create the fleet to aliens."""
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        available_space_x = self.settings.screen_width - (2*alien_width)
+        number_aliens_x = available_space_x//(2*alien_width)
+
+        # Determine the numberof rows of aliens that fit on the screen.
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height - (3*alien_height)-ship_height)
+        number_rows = available_space_y//(2*alien_height)
+
+        # Create the full fleet of aliens.
+        for row_number in range(number_rows):
+            # Create the first row of aliens.
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number,row_number)
+    
+    def _create_alien(self, alien_number,row_number):
+        # Create an alien and place it in the row.
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien.x = alien_width + 2*alien_width*alien_number
+        alien.rect.x = alien.x 
+        alien.rect.y = alien.rect.height +2*alien.rect.height*row_number
+        self.aliens.add(alien)
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
