b7baaa7d7f06591e937256e323709d27678165f6
mewprakarn <51776670+mewprakarn@users.noreply.github.com>
2020-04-18 15:39:51 +0700

    Add reset game speed

--- Alien_invasion.py
+++ Alien_invasion.py
@@ -93,8 +93,23 @@ class AlienInvasion:
 
     def _check_play_button(self,mouse_pos):
         """Start new game when the player click play."""
-        if self.play_button.rect.collidepoint(mouse_pos):
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.stats.game_active:
+            # Reset the game settings
+            self.settings.initialize_dynamic_settings()
+            self.stats.reset_stats()
             self.stats.game_active = True
+
+            # Get rid of any remaining aliens and bullets.
+            self.aliens.empty()
+            self.bullets.empty()
+
+            # Create a new fleet and center the ship.
+            self._create_fleet()
+            self.ship.center_ship()
+
+            # Hide the mouse cursor.
+            pygame.mouse.set_visible(False)
 
     def _check_keydown_events(self,event):
         """Response to keypress"""
@@ -208,6 +223,7 @@ class AlienInvasion:
             sleep(0.5)
         else:
             self.stats.game_active = False
+            pygame.mouse.set_visible(True)
 
     def _check_aliens_bottom(self):
         """Check if any aliens have reached the bottom of the screen."""
--- Setting.py
+++ Setting.py
@@ -23,3 +23,22 @@ class Setting:
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
         
+        # Speed up
+        self.speedup_scale = 1.1
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Initialize settings that change throughout the game."""
+        self.ship_speed = 1.5
+        self.bullet_speed = 3.0
+        self.alien_speed = 1.0
+
+        # fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
+
+    def increase_speed(self):
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
+
